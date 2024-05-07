aabf7a29738be822ac17af7862a463ffcf0587d9
wilson22332 <145476221+wilson22332@users.noreply.github.com>
2024-05-07 10:58:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ import pygame
 pygame.init()
 surface = pygame.display.set_mode((800, 500))
 
-game_state = "start_menu"
 color = (128, 128,128)
 surface.fill(color)
-pygame.display.flip()
-screen_width =800
-screen_height = 500
-def draw_start_menu():
-    surface.fill((0, 0, 0))
-    font = pygame.font.SysFont('arial', 40)
-    title = font.render('My Game', True, (255, 255, 255))
-    start_button = font.render('Start', True, (255, 255, 255))
-    surface.blit(title, (screen_width/2 - title.get_width()/2, screen_height/2 - title.get_height()/2))
-    surface.blit(start_button, (screen_width/2 - start_button.get_width()/2, screen_height/2 + start_button.get_height()/2))
-    pygame.display.update()
+rectangle_position = (400, 300) # Centre of screen
+rectangle_dimension = (100, 100) # Width and Height
+rectangle = pygame.Rect(rectangle_position, rectangle_dimension)
+rectangle_color = (255,0,0)
+
 running = True
 while running:
+    pygame.draw.rect(surface, rectangle_color, rectangle)
+    pygame.display.update()
     for event in pygame.event.get():
-        if game_state == "start_menu":
-            draw_start_menu()
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()  # Get click position
+            if rectangle.collidepoint(x, y):  # Check if click is within rectangle
+                surface.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                print("left mouse button")
+            elif event.button == 2:
+                print("middle mouse button")
+            elif event.button == 3:
+                print("right mouse button")
+            elif event.button == 4:
+                print("mouse wheel up")
+            elif event.button == 5:
+                print("mouse wheel down")
