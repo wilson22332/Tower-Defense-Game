c2e7370f577b0c94edc4aadc5c29faafaa4a8a54
wilson22332 <145476221+wilson22332@users.noreply.github.com>
2024-05-08 10:59:23 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
 import pygame
 
+
+# Initialize Pygame
 pygame.init()
-surface = pygame.display.set_mode((800, 500))
 
-color = (128, 128,128)
-surface.fill(color)
-rectangle_position = (400, 300) # Centre of screen
-rectangle_dimension = (100, 100) # Width and Height
-rectangle = pygame.Rect(rectangle_position, rectangle_dimension)
-rectangle_color = (255,0,0)
+clock = pygame.time.Clock()
 
-running = True
-while running:
-    pygame.draw.rect(surface, rectangle_color, rectangle)
+# Create a Pygame window
+window_size = (800, 600)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption('Tower Defense')
+time = clock.get_time()
+# Create a font object
+font = pygame.font.Font(None, 24)
+
+# Create a surface for the button
+button_surface = pygame.Surface((150, 50))
+
+
+# Create a pygame.Rect object that represents the button's boundaries
+button_rect = pygame.Rect(325, 125, 150, 50)  # Adjust the position as needed
+rgb = (155, 255, 155)
+show_button = True
+
+# Start the main loop
+while True:
+    # Set the frame rate
+    clock.tick(60)
+    # Fill the display with color
+    screen.fill(rgb)
+
+    # Get events from the event queue
+    for event in pygame.event.get():
+        # Check for the quit event
+        if event.type == pygame.QUIT:
+            # Quit the game
+            pygame.quit()
+
+        # Check for the mouse button down event
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # Call the on_mouse_button_down() function
+            if button_rect.collidepoint(event.pos):
+                button_clicked = True
+                if button_clicked == True:
+                    rgb = (155,155,155)
+                    show_button = False
+
+
+
+    # Draw the button on the screen
+    if show_button:
+        screen.blit(button_surface, (button_rect.x, button_rect.y))
+
+    # Update the game state
     pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()  # Get click position
-            if rectangle.collidepoint(x, y):  # Check if click is within rectangle
-                surface.fill((0,0,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                print("left mouse button")
-            elif event.button == 2:
-                print("middle mouse button")
-            elif event.button == 3:
-                print("right mouse button")
-            elif event.button == 4:
-                print("mouse wheel up")
-            elif event.button == 5:
-                print("mouse wheel down")
