e58adfc59a446457c79083c6acecdbe6819053d7
wilson22332 <145476221+wilson22332@users.noreply.github.com>
2024-06-11 10:53:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from background import Background
-from platform import Platform
+
 import math
 
 # Initialize Pygame
@@ -19,11 +19,19 @@ background_rect = background.get_rect()
 tiles = math.ceil(SCREEN_WIDTH/background_width)+1
 
 scroll = 0
-platform = Platform(300,200)
+
 #Frames
 clock = pygame.time.Clock()
 b=Background(50,30)
 
+#platform
+x = 100
+y = 150
+
+# Creating a rect with width
+# and height
+platform_rect = pygame.Rect(x, y, 200, 50)
+platform_vel =5
 rect = pygame.Rect(135, 220, 30, 30)
 vel = 5
 jump_height = 10
@@ -57,7 +65,7 @@ while True:
             run = False
             pygame.quit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if button_rect.collidepoint(event.pos):
                 button_clicked = True
                 if button_clicked and showing_background == False:
@@ -70,12 +78,18 @@ while True:
     if showing_background:
 
         for i in range(0, tiles):
-            screen.blit(platform.image, (platform.rect))
+
             screen.blit(background, (i * background_width + scroll, 0))
             background_rect.x = i * background_width + scroll
             keys = pygame.key.get_pressed()
             rect.centerx = (rect.centerx + (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel) % 300
+            if platform_rect.left >= 300 or platform_rect.left < 100:
+                platform_vel *= -1
 
+                # Adding platform_vel to x
+                # coordinate of our rect
+            platform_rect.left += platform_vel
+            pygame.draw.rect(screen, (255, 0, 0), platform_rect)
             if keys[pygame.K_SPACE] and not is_jumping:
                 is_jumping = True
                 jump_velocity = -jump_height
@@ -95,5 +109,6 @@ while True:
         # reset scroll
         if abs(scroll) > background_width:
             scroll = 0
+
 
     pygame.display.update()
